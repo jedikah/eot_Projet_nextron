ce2057a7e657ec892a1f2956b3ddcd1950a5247b
Anthony <ratovoson81@gmail.com>
2020-02-03 18:52:24 +0300

    Modal details travaux

---
+++ renderer/components/MainComponent/NewWorkComponent/DetailDossier.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
+import orange from "@material-ui/core/colors/orange";
+
+const styles = theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+    color: orange[500],
+    height: "35px",
+    padding: 0,
+    marginLeft: "10px",
+    minWidth: " 500px"
+  },
+  closeButton: {
+    padding: 0,
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: orange[500]
+  }
+});
+
+const DialogTitle = withStyles(styles)(props => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles(theme => ({
+  root: {
+    padding: theme.spacing(2)
+  }
+}))(MuiDialogContent);
+
+const DialogActions = withStyles(theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+    color: orange[800],
+    height: "35px"
+  }
+}))(MuiDialogActions);
+
+const DetailDossier = props => {
+  return (
+    <div>
+      <Dialog aria-labelledby="customized-dialog-title" open={props.open}>
+        <DialogTitle id="customized-dialog-title">
+          Détails du dossier
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>Nom: {props.item.Nom}</Typography>
+          <Typography gutterBottom>Prix: {props.item.Prix}</Typography>
+          <Typography gutterBottom>Titre: {props.item.NomTer}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={props.handleClose}
+            style={{ color: orange[500] }}
+          >
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default DetailDossier;
