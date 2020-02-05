1cd2c8d75ce2eca797927001738449b3a81fccc5
zeke1806 <joeloliviersidy@gmail.com>
2020-02-05 15:38:04 +0300

    Add divider + icon folder liste de dossier

--- renderer/components/MainComponent/NewWorkComponent/AfficherDossier.js
+++ renderer/components/MainComponent/NewWorkComponent/AfficherDossier.js
@@ -3,10 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 import DetailsIcon from "@material-ui/icons/Details";
+import FolderIcon from "@material-ui/icons/Folder";
 
 import AfficherDossierCtn from "../../../redux/containers/AfficherDossierCtn";
 import DetailDossier from "../../../redux/containers/DetailDossierCtn";
@@ -40,6 +43,9 @@ const AffiCherDossier = ({ actions, travaux, clients, selectedTravau }) => {
           return (
             <div key={i}>
               <ListItem alignItems="flex-start">
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
                 <ListItemText
                   primary={client && client.Nom}
                   secondary={
@@ -73,6 +79,7 @@ const AffiCherDossier = ({ actions, travaux, clients, selectedTravau }) => {
                   </Button>
                 </ListItemSecondaryAction>
               </ListItem>
+              <Divider />
             </div>
           );
         })}
