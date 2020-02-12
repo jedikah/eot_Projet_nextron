151dbf4e475d7082c1380b66df43cfcb8bc858fa
zeke1806 <joeloliviersidy@gmail.com>
2020-02-12 11:39:40 +0300

    Ajout du composant planning

--- renderer/components/MainComponent/Planning/index.js
+++ renderer/components/MainComponent/Planning/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  Scheduler,
+  DayView,
+  Appointments
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 const Planning = () => {
-  return <div>page de planning</div>;
+  return (
+    <Scheduler
+      data={[
+        {
+          startDate: "2018-10-31 10:00",
+          endDate: "2018-10-31 11:00",
+          title: "Meeting"
+        },
+        {
+          startDate: "2018-11-01 18:00",
+          endDate: "2018-11-01 19:30",
+          title: "Go to a gym"
+        }
+      ]}
+    >
+      <DayView />
+      <Appointments />
+    </Scheduler>
+  );
 };
 
 export default Planning;
